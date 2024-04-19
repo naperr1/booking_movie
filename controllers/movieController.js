59e2b6b72630f90d969a72cfcc4518263479569f
Phát <97102848+naperr1@users.noreply.github.com>
2024-04-19 22:07:17 +0700

    movie
    update movie

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -3,6 +3,103 @@ import mongoose from "mongoose";
 import Admin from "../models/Admin.js";
 import Movie from "../models/Movie.js";
 
+// export const addMovie = async (req, res, next) => {
+//   if (!req.headers.authorization) {
+//     return res.status(401).json({ message: "Authorization header is missing" });
+//   }
+
+//   const extractedToken = req.headers.authorization.split(" ")[1];
+//   if (!extractedToken || extractedToken.trim() === "") {
+//     return res.status(404).json({ message: "Token Not Found" });
+//   }
+
+//   let adminId;
+
+//   // Verify token
+//   jwt.verify(extractedToken, process.env.SECRET_KEY, (err, decrypted) => {
+//     if (err) {
+//       return res.status(400).json({ message: `${err.message}` });
+//     } else {
+//       adminId = decrypted.id;
+//       return;
+//     }
+//   });
+
+//   // Create new movie
+//   const {
+//     title,
+//     director,
+//     genre,
+//     releaseDate,
+//     duration,
+//     image,
+//     rating,
+//     cast,
+//     plot,
+//   } = req.body;
+
+//   // Check for missing or empty required fields
+//   if (
+//     !title ||
+//     title.trim() === "" ||
+//     !director ||
+//     director.trim() === "" ||
+//     !genre ||
+//     genre.length === 0 ||
+//     !releaseDate ||
+//     !duration ||
+//     !image ||
+//     image.trim() === "" ||
+//     !rating ||
+//     !cast ||
+//     cast.length === 0 ||
+//     !plot ||
+//     plot.trim() === ""
+//   ) {
+//     return res.status(422).json({ message: "Invalid Inputs" });
+//   }
+
+//   let movie;
+//   try {
+//     movie = new Movie({
+//       title,
+//       director,
+//       genre,
+//       releaseDate: new Date(releaseDate),
+//       duration,
+//       image,
+//       rating,
+//       cast,
+//       plot,
+//     });
+
+//     const session = await mongoose.startSession();
+//     session.startTransaction();
+//     await movie.save({ session });
+
+//     const adminUser = await Admin.findById(adminId);
+//     if (!adminUser) {
+//       await session.abortTransaction();
+//       session.endSession();
+//       return res.status(404).json({ message: "Admin not found" });
+//     }
+
+//     adminUser.addedMovies.push(movie);
+//     await adminUser.save({ session });
+//     await session.commitTransaction();
+//     session.endSession();
+//   } catch (err) {
+//     console.error(err);
+//     return res.status(500).json({ message: "Request Failed" });
+//   }
+
+//   if (!movie) {
+//     return res.status(500).json({ message: "Request Failed" });
+//   }
+
+//   return res.status(201).json({ movie });
+// };
+
 export const addMovie = async (req, res, next) => {
   if (!req.headers.authorization) {
     return res.status(401).json({ message: "Authorization header is missing" });
@@ -32,10 +129,13 @@ export const addMovie = async (req, res, next) => {
     genre,
     releaseDate,
     duration,
-    image,
     rating,
     cast,
     plot,
+    reviews,
+    ticketPrice,
+    theaters,
+    image,
   } = req.body;
 
   // Check for missing or empty required fields
@@ -54,7 +154,8 @@ export const addMovie = async (req, res, next) => {
     !cast ||
     cast.length === 0 ||
     !plot ||
-    plot.trim() === ""
+    plot.trim() === "" ||
+    !ticketPrice
   ) {
     return res.status(422).json({ message: "Invalid Inputs" });
   }
@@ -67,10 +168,13 @@ export const addMovie = async (req, res, next) => {
       genre,
       releaseDate: new Date(releaseDate),
       duration,
-      image,
       rating,
       cast,
       plot,
+      reviews,
+      ticketPrice,
+      theaters,
+      image,
     });
 
     const session = await mongoose.startSession();
--- models/Movie.js
+++ models/Movie.js
@@ -1,47 +1,18 @@
 import mongoose from "mongoose";
 
 const movieSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-  },
-  director: {
-    type: String,
-    required: true,
-  },
-  genre: [
-    {
-      type: String,
-      required: true,
-    },
-  ],
-  releaseDate: {
-    type: Date,
-    required: true,
-  },
-  duration: {
-    type: Number,
-    required: true,
-  },
-  image: {
-    type: String,
-    required: true,
-  },
-
-  rating: {
-    type: Number,
-    required: true,
-  },
-  cast: [
-    {
-      type: String,
-      required: true,
-    },
-  ],
-  plot: {
-    type: String,
-    required: true,
-  },
+  title: { type: String, required: true },
+  director: { type: String, required: true },
+  genre: [{ type: String, required: true }],
+  releaseDate: { type: Date, required: true },
+  duration: { type: Number, required: true },
+  rating: { type: Number, required: true },
+  cast: [{ type: String, required: true }],
+  plot: { type: String, required: true },
+  reviews: { type: String },
+  ticketPrice: { type: Number, required: true },
+  theaters: { type: String },
+  image: { type: String, required: true },
   bookings: [{ type: mongoose.Types.ObjectId, ref: "Booking" }],
 });
 
